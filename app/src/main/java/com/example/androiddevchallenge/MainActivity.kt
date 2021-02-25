436b71e288d6c59220412a1866f3cab622a80170
suhas mandrawadkar <suhas.sm@gmail.com>
2021-02-26 03:53:54 +1100

    [Week1] Compose Challenge - fix spotless warnings

--- app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
+++ app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
@@ -20,11 +20,19 @@ import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.*
+import androidx.compose.material.Button
+import androidx.compose.material.Card
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -35,7 +43,11 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import androidx.navigation.NavType
-import androidx.navigation.compose.*
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.navArgument
+import androidx.navigation.compose.navigate
+import androidx.navigation.compose.rememberNavController
 import com.example.androiddevchallenge.ui.theme.MyTheme
 import com.example.pets.data.Dog
 
@@ -64,10 +76,12 @@ fun AppNavigator() {
 
     NavHost(navController = navigator, startDestination = "home") {
         composable("home") { HomeScreen(com.example.pets.data.Pets.dogs, navigator) }
-        composable("details/{dog}",
+        composable(
+            "details/{dog}",
             arguments = listOf(
                 navArgument("dog") { type = NavType.StringType }
-            )) { backStackEntry ->
+            )
+        ) { backStackEntry ->
             backStackEntry.arguments?.getString("dog")
                 ?.let { DetailScreen(dogName = it, navHostController = navigator) }
         }
@@ -119,9 +133,11 @@ fun PetList(pets: List<Dog>, modifier: Modifier = Modifier, navHostController: N
 
 @Composable
 fun PetItem(pet: Dog, modifier: Modifier = Modifier, navHostController: NavHostController) {
-    Card(modifier = modifier
-        .fillMaxWidth()
-        .clickable { navHostController.navigate("details/${pet.name}") }) {
+    Card(
+        modifier = modifier
+            .fillMaxWidth()
+            .clickable { navHostController.navigate("details/${pet.name}") }
+    ) {
         Column(modifier = modifier.padding(8.dp)) {
             Image(
                 painter = painterResource(id = pet.resId),
--- app/src/main/java/com/example/androiddevchallenge/data/PetsData.kt
+++ app/src/main/java/com/example/androiddevchallenge/data/PetsData.kt
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package com.example.pets.data
 
 import androidx.annotation.DrawableRes
@@ -27,17 +26,17 @@ data class Dog(
 
 object Pets {
     val dogs = listOf<Dog>(
-        Dog(R.drawable.doge,"Tom", "These cute pups are quite versatile and can adapt to both apartment living or a home with a backyard to run in. Though their short, stout body types may lead you to believe they are lazy, these dogs will get bursts of energy and enjoy a game of fetch or a trip to the dog park."),
-        Dog(R.drawable.happy,"Jack", "These make for good guard dogs, and they’re alert to strangers. The mixed breed is very energetic and athletic, so access to a safe outdoor space is preferred."),
-        Dog(R.drawable.hat,"Russell", "Playful"),
-        Dog(R.drawable.doge,"Stuart", "Cute"),
-        Dog(R.drawable.happy,"Dan", "Aggressive"),
-        Dog(R.drawable.hat,"Bill", "Playful"),
-        Dog(R.drawable.doge,"Ben", "Cute"),
-        Dog(R.drawable.happy,"Nathan", "Aggressive"),
-        Dog(R.drawable.hat,"Johnnie", "Playful"),
-        Dog(R.drawable.doge,"Frank", "Cute"),
-        Dog(R.drawable.happy,"Lily", "Aggressive"),
-        Dog(R.drawable.hat,"Johnnie", "Playful")
+        Dog(R.drawable.doge, "Tom", "These cute pups are quite versatile and can adapt to both apartment living or a home with a backyard to run in. Though their short, stout body types may lead you to believe they are lazy, these dogs will get bursts of energy and enjoy a game of fetch or a trip to the dog park."),
+        Dog(R.drawable.happy, "Jack", "These make for good guard dogs, and they’re alert to strangers. The mixed breed is very energetic and athletic, so access to a safe outdoor space is preferred."),
+        Dog(R.drawable.hat, "Russell", "Playful"),
+        Dog(R.drawable.doge, "Stuart", "Cute"),
+        Dog(R.drawable.happy, "Dan", "Aggressive"),
+        Dog(R.drawable.hat, "Bill", "Playful"),
+        Dog(R.drawable.doge, "Ben", "Cute"),
+        Dog(R.drawable.happy, "Nathan", "Aggressive"),
+        Dog(R.drawable.hat, "Johnnie", "Playful"),
+        Dog(R.drawable.doge, "Frank", "Cute"),
+        Dog(R.drawable.happy, "Lily", "Aggressive"),
+        Dog(R.drawable.hat, "Johnnie", "Playful")
     )
 }
